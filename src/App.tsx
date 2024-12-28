e66bee7835bf6849e43d75c12db60f8345619156
Yash Chauhan <forcourse1234@gmail.com>
2024-12-28 16:48:10 +0530

    Implement inventory fetching and display in CardContainer component
    - Added useEffect in App component to fetch inventory data on mount and update the inventory store.
    - Enhanced CardContainer to calculate and display total products, total store value, out-of-stock items, and number of categories based on the fetched inventory data.
    - Updated imports to use alias paths for better readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import CardContainer from "./components/CardContainer";
+import { useEffect } from "react";
+import CardContainer from "components/CardContainer";
 import Layout from "./Layout";
+import { getInventory } from "network";
+import { useInventoryStore } from "./store/inventory.store";
 
 function App() {
+  const update = useInventoryStore((state) => state.update);
+
+  useEffect(() => {
+    (async () => {
+      const [data, error] = await getInventory();
+      if (error || !data) return;
+      update("inventory", data);
+    })();
+    return () => {};
+  }, []);
+
   return (
     <Layout>
       <div className="h-full bg-primary px-10 pt-5">
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -4,15 +4,55 @@ import {
   PackageX,
   ShoppingCartIcon,
 } from "lucide-react";
-import Card from "./Card";
+import Card from "components/Card";
+import { useInventoryStore } from "store/inventory.store";
+import { useMemo } from "react";
+import { TGetInventory } from "src/types";
 
 const CardContainer = () => {
+  const inventory = useInventoryStore((state) => state.inventory);
+
+  const { outOfStock, numberOfCategory } = useMemo(() => {
+    let outOfStock = 0;
+    let numberOfCategory = 0;
+
+    if (inventory.length === 0) return { outOfStock, numberOfCategory };
+
+    const groupByCategory = Object.groupBy(
+      inventory,
+      (element: TGetInventory) => element.category
+    );
+
+    numberOfCategory = Object.keys(groupByCategory).length;
+
+    for (let i = 0; i < inventory.length; i++) {
+      if (inventory[i].quantity === 0) outOfStock++;
+    }
+
+    return { outOfStock, numberOfCategory };
+  }, [inventory]);
+
   return (
     <div className="flex gap-2 mt-5">
-      <Card Icon={ShoppingCartIcon} heading="Total product" stats={123123} />
-      <Card Icon={BadgeDollarSign} heading="Total store value" stats={123123} />
-      <Card Icon={PackageX} heading="Out of stock" stats={123123} />
-      <Card Icon={Layers} heading="No of Category" stats={123123} />
+      <Card
+        Icon={ShoppingCartIcon}
+        heading="Total product"
+        stats={inventory.length}
+      />
+      <Card
+        Icon={BadgeDollarSign}
+        heading="Total store value"
+        stats={inventory.reduce(
+          (prev, current) => prev + Number(current?.price ?? 0),
+          0
+        )}
+      />
+      <Card
+        Icon={PackageX}
+        heading="Out of stock"
+        stats={outOfStock > 0 ? outOfStock : "-"}
+      />
+      <Card Icon={Layers} heading="No of Category" stats={numberOfCategory} />
     </div>
   );
 };
