d90026f4ea555108f2e6cf39b8232b720f9c02cd
Yash Chauhan <forcourse1234@gmail.com>
2024-12-28 16:44:21 +0530

    added alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: resolve(root, "components"),
+      utils: resolve(root, "utils"),
+      network: resolve(root, "network"),
+      store: resolve(root, "store"),
+    },
+  },
+});
