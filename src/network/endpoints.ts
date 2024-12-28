662b56ec4dcd61cc4e551ea0aac3ca4eb1e9ccbd
Yash Chauhan <forcourse1234@gmail.com>
2024-12-28 16:45:54 +0530

    Add network module with inventory endpoints and request function

---
+++ src/network/endpoints.ts
@@ -0,0 +1,5 @@
+// This file contains endpoints for the application
+
+export default {
+  inventory: "/inventory",
+};
---
+++ src/network/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This file exports all the requests functions defined under file named
+ * [name].api.ts
+ * in the request directory
+ */
+
+export * from "./request/inventory.api";
