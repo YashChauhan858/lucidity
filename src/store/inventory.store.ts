fc41140960e020bd04adea5d035b9a9181ce8aa6
Yash Chauhan <forcourse1234@gmail.com>
2024-12-28 16:47:27 +0530

    Enhance inventory store: add inventory array and update method for state management

--- src/store/inventory.store.ts
+++ src/store/inventory.store.ts
@@ -1,19 +1,21 @@
+import { Actions, State } from "src/types/store.types";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-type State = {
-  isUser: boolean;
-};
-
-type Actions = {
-  toggle: () => void;
-};
 
 export const useInventoryStore = create<State & Actions>()(
   immer((set) => ({
     isUser: false,
+    inventory: [],
     toggle: () =>
       set((state) => {
         state.isUser = !state.isUser;
       }),
+    update: (key: keyof State, value: State[keyof State]) => {
+      set((state) => {
+        if (key in state) {
+          state[key] = value as any;
+        }
+      });
+    },
   }))
 );
